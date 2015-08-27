265078cbfa00c095d384c314db4bafb95372b286
Federico Donnarumma <federico.donnarumma@mulesoft.com>
2015-08-26 21:00:36 -0300

    Haciendo cambios para TP 2

---
+++ TP2/src/ejercicio2/Ejemplo2.java
@@ -0,0 +1,11 @@
+package ejercicio2;
+
+/**
+ * Created by federicodonnarumma on 8/26/15.
+ */
+public class Ejemplo2 {
+
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+    }
+}
